0ffc06574038cd404aaa88e39f25b5a200858ce5
Mayamba10 <mayambarubangs@gmail.com>
2025-11-29 19:42:51 +0100

    Add files via upload

---
+++ num.py
@@ -0,0 +1,8 @@
+def Absolute_value(num):
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+print("Absolute value of ", 67, " is ", Absolute_value(67))
+print("Absolute value of -69 is ", Absolute_value(-69))
